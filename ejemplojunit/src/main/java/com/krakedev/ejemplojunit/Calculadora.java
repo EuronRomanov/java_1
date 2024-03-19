237be0326f3f9cd82a0120064bd1e6100b9a8c54
EuronRomanov <juanpablo.gaona@yahoo.com>
2024-03-19 11:34:37 -0500

    reto 30

---
+++ ejemplojunit/src/main/java/com/krakedev/ejemplojunit/Calculadora.java
@@ -0,0 +1,12 @@
+package com.krakedev.ejemplojunit;
+
+public class Calculadora {
+public int sumar (int numero1, int numero2) {
+	return numero1+numero2;
+}
+
+public int restar(int i, int j) {
+	// TODO Auto-generated method stub
+	return i-j;
+}
+}
